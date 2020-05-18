dc164aa7f17a87f79cb8c372566ddf96ee0cd0fb
ahmetgaraev <ahmetgaraev@gmail.com>
2020-05-18 15:25:08 +0300

    Change method send new data

--- data_sendler.js
+++ data_sendler.js
@@ -15,6 +15,28 @@ function getNewData() {
     return params
 }
 
+function sendData(data) {
+    let post_options = {
+        host: 'localhost',
+        port: '3000',
+        path: '/update',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(data)
+        }
+    };
+
+    let post_req = http.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+    post_req.write(data);
+    post_req.end();
+}
+
 setInterval( () => {
-    http.get('http://localhost:3000/update?' + getNewData());
+    sendData(getNewData());
 }, 100)
